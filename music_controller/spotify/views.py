17860a3588f392fad092ea50071c820fdf6815d4
Klaidas Markisius <markisius.klaidas@gmail.com>
2023-03-31 15:51:27 +0300

    -

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -5,6 +5,8 @@ from requests import Request, post
 from rest_framework.response import Response
 from rest_framework import status
 from  .util import update_or_create_user_tokens, is_spotify_authenticated
+from ..api.models import Room
+
 
 # Create your views here.
 class AuthURL(APIView):
@@ -48,3 +50,11 @@ class IsAuthenticated(APIView):
     def get(self, request, format=None):
         is_authenticated = is_spotify_authenticated(self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code[0])
+        host = room.host
+        endpoint = "/player/currently-playing"
